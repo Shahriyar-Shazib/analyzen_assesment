aaeb2dfd4564628f58550ab1577f44b8b6c8e432
shahriyar <shazib.shahriyar@gmail.com>
2024-10-21 22:37:33 +0600

    update

--- resources/views/users/userIndex.blade.php
+++ resources/views/users/userIndex.blade.php
@@ -7,8 +7,10 @@
             {{ __('User List') }}
         </h2>
         <div class = "d-flex">
-
-            <a class = "btn btn-sm  btn-info " style = "color:white;"href="{{route('user.create')}}">create</a>
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Create
+        </button>
+            <!-- <a class = "btn btn-sm  btn-info " style = "color:white;"href="{{route('user.create')}}">create</a> -->
         </div>
     </div>
         <div class="pl-10 py-12 table">
@@ -36,6 +38,48 @@
                     
                 </tbody>
             </table>
-        </dib>
+        </div>
     </div>
+<!-- Create Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">User Create</h5>
+       
+      </div>
+      <form action="" id="user_create_form" method="post">
+        <div class="modal-body">
+            
+                @csrf
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Name</span>
+                    <input type="text" class="form-control" name ="name" aria-label="Enter your name" aria-describedby="inputGroup-sizing-default">
+                </div>
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="inputGroup-sizing-default">@</span>
+                    <input type="text" class="form-control" name ="email" aria-label="Enter your email" aria-describedby="inputGroup-sizing-default">
+                </div>
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="inputGroup-sizing-default">password</span>
+                    <input type="text" class="form-control" name ="password" aria-label="Enter your email" aria-describedby="inputGroup-sizing-default">
+                </div>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="submit" id="submit" class="btn btn-primary">Save</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+@section('js')
+   <script>
+    $('#user_create_form')on('submit',function(event){
+        event.preventDefault();
+        alert(123);
+    })
+   </script>
+@endsection
 </x-app-layout>
+
